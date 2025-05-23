55223ce140ac5f6d5b593a5115ee35d00e6004f2
II-SARIP-II <paris.gougne.05@gmail.com>
2025-05-23 19:34:59 +0200

    bluesky

--- bot.py
+++ bot.py
@@ -1,19 +1,17 @@
-import tweepy
-import csv
-import random
 import os
+import csv
+from atproto import Client
 
-api_key = os.getenv("NfRvEqpn6nB7BbAVawZ7r7eBa")
-api_secret = os.getenv("ol0gueUCp5F4FvOI2IuQQQQZufQCyxBHKjI9yaJ1HzPEd2sZxI")
-access_token = os.getenv("1925570142198452224-I6Px9mavIco11kB9XMeeafzEglgdJh")
-access_token_secret = os.getenv("Y8dYl6HZ6RTntMjcrs40AoLhvKU8Ey8cDaafMalPO4coh")
+# Charger les identifiants depuis les variables d’environnement ou en dur ici
+HANDLE = "united-europe.bsky.social"
+APP_PASSWORD = "bxhv-pcba-c7p5-leza"
 
-auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
-api = tweepy.API(auth)
+# Init client
+client = Client()
+client.login(HANDLE, APP_PASSWORD)
 
-# Chargement des tweets depuis le CSV
-def get_next_tweet_from_csv(filename="tweets.csv", index_file="index.txt"):
-    # Lecture de l'index courant
+# Fonction pour lire le prochain tweet à partir d’un CSV
+def get_next_post_from_csv(filename="posts.csv", index_file="index.txt"):
     if os.path.exists(index_file):
         with open(index_file, "r") as f:
             try:
@@ -23,24 +21,21 @@ def get_next_tweet_from_csv(filename="tweets.csv", index_file="index.txt"):
     else:
         index = 0
 
-    # Lecture des tweets dans le CSV
     with open(filename, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
-        tweets = [row[0] for row in reader if row]
+        posts = [row[0] for row in reader if row]
 
-    if not tweets:
+    if not posts:
         raise ValueError("Le fichier CSV est vide ou mal formaté.")
 
-    # Récupérer le tweet à l'index courant (modulo pour boucle)
-    tweet = tweets[index % len(tweets)]
+    post = posts[index % len(posts)]
 
-    # Mettre à jour l'index pour la prochaine exécution
     with open(index_file, "w") as f:
-        f.write(str((index + 1) % len(tweets)))
+        f.write(str((index + 1) % len(posts)))
 
-    return tweet
+    return post
 
-# Poster le tweet
-tweet = get_next_tweet_from_csv()
-api.update_status(tweet)
-print("Tweet envoyé :", tweet)
+# Poster sur Bluesky
+post = get_next_post_from_csv()
+client.send_post(text=post)
+print("Post envoyé sur Bluesky :", post)
